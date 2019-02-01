b04ad13338ff7fd43a36c710f925017d9eed530d
challabhanuteja <47059570+challabhanuteja@users.noreply.github.com>
2019-02-01 17:14:22 +0530

    Update spellchecker.c

--- spellchecker.c
+++ spellchecker.c
@@ -4,46 +4,34 @@ int strcompare(char a[],char b[])
 {
 	int x=strlen(a)-1,i;
 	int y=strlen(b),k=0;
-	//printf("%d %d\n",x,y);
 	if(x!=y)
 	return 0;
 	else
 	{
 		for(i=0;i<x;i++)
 		{
-			//k++;
-			//printf("%d ",k);
 			if(a[i]!=b[i])
-			return 0;
-			
+			return 0;	
 		}
 		
 		return 1;
 	}
-	//return 0;
 }
 void suggestions(char a[])
 {
 	FILE *d;int n=strlen(a);char ch[10];
-	d=fopen("C:/Users/BHANU/Desktop/DS project/dictionary1000.txt","r");
+	//dictionary1000.txt is the text file in which 1000 word with correct spelling are stored. 
+	//dictionary1000.txt is also uploaded along with this source code.
+	//this project not only works with 1000 words you can add any number of words in that text file, it will work just fine.
+	d=fopen("C:/Users/BHANU/Desktop/DS project/dictionary1000.txt","r");//here paste the location of dictionary1000.txt in ur computer.
 	while(!feof(d))
 	{
 		fgets(ch,10,d);
-		//printf("%d %d\n",strlen(a),strlen(ch));
-		//printf("%c ",ch[0]);
 		if(strlen(a)+1==strlen(ch))
 		{
-			//printf("xxxxxxxxxxxxxxxxx%d ",strcmp(a,ch));
-			//printf("xxxxxxxxxxxxxxxx");
-			//printf("%d %d",strlen(a),strlen(ch));
 				if(a[0]==ch[0])
 				printf("%s",ch);
-				
-		}
-//		else if(a[0]<ch[0])
-//		{
-//			break;	
-//		}	
+		}	
 	}
 }
 
@@ -51,22 +39,14 @@ int main()
 {
 	char ch[10],sentence[1000];int temp=0,i=0,n,k=0,s=0,l=0;
 	FILE *d;
-	d=fopen("C:/Users/BHANU/Desktop/DS project/dictionary1000.txt","r");
+	d=fopen("C:/Users/BHANU/Desktop/DS project/dictionary1000.txt","r");//here paste the location of dictionary1000.txt in ur computer.
 	printf("Enter any sentence:");
 	scanf("%[^\n\t]s",sentence);
 	n=strlen(sentence);
-	//printf("%d",n);
-//	for(i=0;i<n;i++)
-//	{
-//		if(sentence[i]==' ')
-//		s++;
-//	}
 	i=0;
 printf("Suggestion:\n");
 while(i<n)
 	{
-	
-	
 		char t[10];int j=0;
 		for(;sentence[i]!=' ';i++,j++)
 		{
@@ -80,11 +60,8 @@ while(i<n)
 			
 		}t[j]='\0';
 		i++;
-		//printf("%s\n",t);
-		
-		
 		k=0;
-		d=fopen("C:/Users/BHANU/Desktop/DS project/dictionary1000.txt","r");
+		d=fopen("C:/Users/BHANU/Desktop/DS project/dictionary1000.txt","r");//here paste the location of dictionary1000.txt in ur computer.
 	while(!feof(d))
 	{
 		fgets(ch,10,d);
@@ -102,14 +79,6 @@ while(i<n)
 		}
 		
 	}
-	
-
-	/*//	if(k==0)
-//	printf("correct ");
-//	else
-//	printf("error ");
-	NEXT MODULE STARTS FROM HERE*/
-	
 	if(k!=0)
 	{
 		printf("for %s\n",t);
@@ -120,14 +89,7 @@ while(i<n)
 	
 	
 }
- if(s==0)
+if(s==0)
  printf("No Suggestions");
-//while(!feof(d))
-//	{
-//		printf("%s",fgets(ch,10,d));
-//		k++;
-//		printf("%d ",k);
-//		
-//	}
 return 0;	
 }
